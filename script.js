bb80ba9fd92229e528cfffbcc45273c14ea9972b
Madhura Tatte <madhuratatte95@gmail.com>
2022-12-15 13:46:26 +0530

    all task complete day-2

---
+++ script.js
@@ -0,0 +1,19 @@
+let arr=[
+    "https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LS-Uber-HP-Desktop-HeroBanner1-14Dec2022.gif",
+    "https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LS-Uber-HP-Desktop-HeroBanner6-14Dec2022.gif",
+    "https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LS-Uber-HP-Desktop-HeroBanner2-14Dec2022.jpg",
+    "https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LS-Uber-HP-Desktop-HeroBanner4-14Dec2022.jpg",
+    "https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LS-Uber-HP-Desktop-HeroBanner3-14Dec2022.jpg",
+    "https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LS-Uber-HP-Desktop-HeroBanner5-14Dec2022.jpg"
+    
+];
+
+let val=0;
+setInterval(()=>{
+    let img=document.createElement("img");
+    img.setAttribute("src",arr[val]);
+    document.getElementById("slider").innerHTML=null;
+    document.getElementById("slider").append(img);
+    val++;
+    if(val==arr.length) val=0;
+},10000);
